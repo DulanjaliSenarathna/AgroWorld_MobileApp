1b42371f925cd05f983793ee23b0b1d3a1514480
KandabadaSenarathna <59603716+KandabadaSenarathna@users.noreply.github.com>
2020-01-19 13:52:30 +0530

    Splash Sceen Working

---
+++ app/src/main/java/com/example/dulanjali/agroworld/SplashScreen.java
@@ -0,0 +1,22 @@
+package com.example.dulanjali.agroworld;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this,MainActivity.class));
+            }
+        },5000);
+
+    }
+}
