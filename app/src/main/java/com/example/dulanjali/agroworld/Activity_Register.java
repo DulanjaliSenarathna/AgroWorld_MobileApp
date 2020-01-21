0638a1d0ecb1c65e047947fadb538d5d34e8679e
KandabadaSenarathna <59603716+KandabadaSenarathna@users.noreply.github.com>
2020-01-21 09:02:29 +0530

    Register Activity UI Completed

---
+++ app/src/main/java/com/example/dulanjali/agroworld/Activity_Register.java
@@ -0,0 +1,13 @@
+package com.example.dulanjali.agroworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__register);
+    }
+}
