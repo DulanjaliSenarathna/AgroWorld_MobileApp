9b6658a50b3f64b0de020edff622620ad7b581af
KandabadaSenarathna <59603716+KandabadaSenarathna@users.noreply.github.com>
2020-02-07 13:43:10 +0530

    Update Profile UI completed

--- app/src/main/java/com/example/dulanjali/agroworld/Activity_Dashboard.java
+++ app/src/main/java/com/example/dulanjali/agroworld/Activity_Dashboard.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class Activity_Dashboard extends AppCompatActivity {
 
     Button logout;
+    ImageView updateProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +24,16 @@ public class Activity_Dashboard extends AppCompatActivity {
                     @Override
                     public void onClick(View v) {
                         startActivity(new Intent(Activity_Dashboard.this,Activity_Login.class));
+                    }
+                }
+        );
+
+        updateProfile = findViewById(R.id.update_profile);
+        updateProfile.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(Activity_Dashboard.this,Activity_Profile.class));
                     }
                 }
         );
--- app/src/main/java/com/example/dulanjali/agroworld/Activity_Profile.java
+++ app/src/main/java/com/example/dulanjali/agroworld/Activity_Profile.java
@@ -2,13 +2,20 @@ package com.example.dulanjali.agroworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Activity_Profile extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__profile);
+
+
     }
 }
