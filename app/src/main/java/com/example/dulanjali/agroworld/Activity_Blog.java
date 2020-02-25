86e3d1633da2409fc2eee941b7cb4fac96c0fd40
KandabadaSenarathna <59603716+KandabadaSenarathna@users.noreply.github.com>
2020-02-25 20:34:46 +0530

    Search Post Working

--- app/src/main/java/com/example/dulanjali/agroworld/Activity_Blog.java
+++ app/src/main/java/com/example/dulanjali/agroworld/Activity_Blog.java
@@ -7,19 +7,30 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.bumptech.glide.Glide;
+import com.example.dulanjali.agroworld.Interface.ItemClickListner;
 import com.example.dulanjali.agroworld.Model.Post;
 import com.example.dulanjali.agroworld.ViewHolder.PostViewHolder;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.mancj.materialsearchbar.MaterialSearchBar;
 import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Activity_Blog extends AppCompatActivity {
 
@@ -28,14 +39,62 @@ public class Activity_Blog extends AppCompatActivity {
     private RecyclerView recyclerView;
     RecyclerView.LayoutManager layoutManager;
 
+    FirebaseRecyclerAdapter<Post,PostViewHolder> searchAdapter;
+    MaterialSearchBar materialSearchBar;
+    List<String> suggestList = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__blog);
 
-
         addPostbtn = findViewById(R.id.floatingActionButton);
 
+        materialSearchBar = (MaterialSearchBar)findViewById(R.id.searchBar);
+        materialSearchBar.setHint("Enter Post Title");
+
+        materialSearchBar.setCardViewElevation(10);
+        materialSearchBar.addTextChangeListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                List<String> suggest = new ArrayList<String>();
+                for(String search:suggestList)
+                {
+                    if(search.toLowerCase().contains(materialSearchBar.getText().toLowerCase()))
+                        suggest.add(search);
+                }
+                materialSearchBar.setLastSuggestions(suggest);
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        materialSearchBar.setOnSearchActionListener(new MaterialSearchBar.OnSearchActionListener() {
+            @Override
+            public void onSearchStateChanged(boolean enabled) {
+                if(!enabled)
+                    onStart();
+            }
+
+            @Override
+            public void onSearchConfirmed(CharSequence text) {
+                startSearch(text);
+
+            }
+
+            @Override
+            public void onButtonClicked(int buttonCode) {
+
+            }
+        });
         mDatabase = FirebaseDatabase.getInstance().getReference().child("Posts");
 
 
@@ -51,7 +110,43 @@ public class Activity_Blog extends AppCompatActivity {
         recyclerView.setHasFixedSize(true);
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
+
     }
+
+    private void startSearch(CharSequence text) {
+
+        Query searchByName = mDatabase.orderByChild("title").equalTo(text.toString());
+        FirebaseRecyclerOptions<Post> options = new FirebaseRecyclerOptions.Builder<Post>()
+                .setQuery(searchByName,Post.class)
+                .build();
+
+
+
+        searchAdapter = new FirebaseRecyclerAdapter<Post, PostViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull PostViewHolder holder, int position, @NonNull Post model) {
+                Picasso.get().load(model.getImage()).into(holder.imageView);
+                holder.textUserName.setText(model.getUser_name());
+                holder.textTime.setText(model.getTime());
+                holder.textTitle.setText(model.getTitle());
+                holder.textDescription.setText(model.getDescription());
+                Picasso.get().load(model.getuDp()).into(holder.uDp);
+            }
+
+
+            @NonNull
+            @Override
+            public PostViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View itemView = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.blog_row,parent,false);
+                return new PostViewHolder(itemView);
+            }
+        };
+        searchAdapter.startListening();
+        recyclerView.setAdapter(searchAdapter);
+    }
+
+
 
     @Override
     protected void onStart() {
